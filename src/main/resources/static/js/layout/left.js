8dbf5de72e86aef0e74ba0acfa0292356a36852a
songnumber1 <songnumber1@naver.com>
2021-11-17 18:13:52 +0900

    Date  - 2021-11-17 18:13
    Content
     - 메뉴 가져와 출력
     - 세번째 dept 메뉴 오류 있음
    
    Todo
     - Storage 서버 통신
     - Sub, Last Menu 추가
     - 메뉴 활성화 및 수정 또는 삭제 가능하도록 Grid 화면 필요
     - 좌측 메뉴에 맞는 Content 디자인 및 구성
     - footer 설정
     - storage 보기, 즐겨찾기 기능 구현
     - 좌측 메뉴 json으로 가져온 데이터로 구성하기
     - 세번째 dept 메뉴 오류 있음

---
+++ src/main/resources/static/js/layout/left.js
@@ -0,0 +1,120 @@
+$(document).ready(function() {
+    let menulist = JSON.parse(document.getElementById("menulist").innerHTML);
+
+    $("#container-menulist").empty();
+    $("#container-menulist").append('');
+
+    var html = "";
+    
+    for (category in menulist) {
+        html = html + '<div class="sb-sidenav-menu-heading">' + menulist[category].categoryTitle + '</div>';
+
+        var oneDeptMenus = menulist[category].menuOneDepts;
+
+        for(oneDeptkey in oneDeptMenus) {
+            console.log(oneDeptMenus[oneDeptkey]);
+            var twoDeptMenus = oneDeptMenus[oneDeptkey].menuTwoDepts;
+            var oneDeptMenuId = oneDeptMenus[oneDeptkey].menuOneDeptid;
+            var oneDeptMenuUrl = oneDeptMenus[oneDeptkey].menuOneDeptUrl;
+            var oneDeptMenuIcon = oneDeptMenus[oneDeptkey].menuOneDeptIcon;
+            var oneDeptMenuTitle = oneDeptMenus[oneDeptkey].menuOneDeptTitle;
+            var oneDeptMenuAria = oneDeptMenuTitle + '_' + oneDeptMenuId;
+            
+            //console.log(replaceAllFunc(uuidFunc(), '-', ''));
+           
+            if(twoDeptMenus.length === 0) {
+                html = html + 
+                    `<a class="nav-link" href="` + oneDeptMenuUrl + `">
+                        <div class="sb-nav-link-icon">
+                            <i class="` + oneDeptMenuIcon + `"></i>
+                        </div> 
+                        ` + oneDeptMenuTitle + `
+                    </a>`;
+            }
+            else {
+                html = html + 
+                    `<a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#` + oneDeptMenuAria + `" aria-expanded="false"
+                        aria-controls="` +  oneDeptMenuAria + `">
+                        <div class="sb-nav-link-icon">
+                            <i class="` + oneDeptMenuIcon + `"></i>
+                        </div>` + oneDeptMenuTitle + `
+                        <div class="sb-sidenav-collapse-arrow">
+                            <i class="fas fa-angle-down"></i>
+                        </div>
+                    </a>`;
+            }
+
+            for(twoDeptkey in twoDeptMenus) {
+                var threeDeptMenus = twoDeptMenus[twoDeptkey].menuThreeDepts;
+                var twoDeptMenuId = twoDeptMenus[twoDeptkey].menuTwoDeptid;
+                var twoDeptMenuUrl = twoDeptMenus[twoDeptkey].menuTwoDeptUrl;
+                var twoDeptMenuTitle = twoDeptMenus[twoDeptkey].menuTwoDeptTitle;
+                var twoDeptMenuAria = twoDeptMenuTitle + '_' + twoDeptMenuId;
+                var isInsertThreeDeptMenu = false;
+
+                if(threeDeptMenus.length === 0) {
+                    html = html + 
+                        `<div class="collapse" id="` + oneDeptMenuAria + `" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                            <nav class="sb-sidenav-menu-nested nav">
+                                <a class="nav-link" href="` +  twoDeptMenuUrl + `">
+                                    ` +  twoDeptMenuTitle + `
+                                </a> 
+                            </nav>
+                        </div>`;
+                }
+                else {
+                    var threeHtml = '';
+
+                    for(threeDeptkey in threeDeptMenus) {                        
+                        threeHtml = threeHtml + 
+                            `<a class="nav-link" href="` + threeDeptMenus[threeDeptkey].menuThreeDeptUrl + `">` + 
+                                threeDeptMenus[threeDeptkey].menuThreeDeptTitle + 
+                            `</a>`;
+                    }
+                    
+                    if(!isInsertThreeDeptMenu) {
+                        html = html + `
+                            <div class="collapse" id="` +  oneDeptMenuAria + `" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
+                                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#` + twoDeptMenuAria + 
+                                    `" aria-expanded="false" aria-controls="` + twoDeptMenuAria + `">
+                                        ` + twoDeptMenuTitle + `
+                                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                                    </a>
+                                    <div class="collapse" id="` + twoDeptMenuAria+ `" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
+                                        <nav class="sb-sidenav-menu-nested nav">
+                                        ` + threeHtml + `
+                                        </nav>
+                                    </div>
+                                </nav>
+                            </div>
+                        `;
+
+                        isInsertThreeDeptMenu = true;
+                    } 
+                    else {
+                        html = html + `
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#` + twoDeptMenuAria + 
+                            `" aria-expanded="false" aria-controls="` + twoDeptMenuAria + `">
+                                ` + twoDeptMenuTitle + `
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="` + twoDeptMenuAria+ `" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                ` + threeHtml + `
+                                </nav>
+                            </div>
+                        `;
+                    }
+                    
+                    html = html + `
+                            </nav>
+                        </div>
+                    `;
+                }
+            }
+        }
+	}
+
+    $("#container-menulist").append(html);
+});
--- src/main/webapp/WEB-INF/views/layout/left.jsp
+++ src/main/webapp/WEB-INF/views/layout/left.jsp
@@ -1,12 +1,19 @@
+<div id="menulist" style="display: none;">
+	${category}
+</div>
+
 <div id="layoutSidenav">
 	<div id="layoutSidenav_nav">
 		<nav class="sb-sidenav accordion sb-sidenav-dark"
 			id="sidenavAccordion">
 			<div class="sb-sidenav-menu">
-				<div class="nav">
-					<!-- Profile -->
-					<div class="sb-sidenav-menu-heading">Profile</div>
+				<div class="nav" id="container-menulist">
 
+					
+
+					<!--
+					<div class="sb-sidenav-menu-heading">Profile</div>
+					
 					<a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseProfile" aria-expanded="false"
 						aria-controls="collapseProfile">
 						<div class="sb-nav-link-icon">
@@ -26,9 +33,7 @@
 					</div>
 					
 					<hr/>
-					<!-- ========================================================================================================== -->
 
-					<!-- Data -->
 					<div class="sb-sidenav-menu-heading">Data</div>	
 					
 					<a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePrograms" aria-expanded="false" aria-controls="collapsePrograms">
@@ -36,6 +41,7 @@
                         Programs
                         <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                     </a>
+
                     <div class="collapse" id="collapsePrograms" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
                         <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
                             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#programsCollapseDB" aria-expanded="false" aria-controls="programsCollapseDB">
@@ -78,9 +84,7 @@
 
 					<hr/>
 
-					<!-- ========================================================================================================== -->
 					
-					<!-- Odds and Ends -->
 					<div class="sb-sidenav-menu-heading">Etc</div>
 					<a class="nav-link" href="#">
 						<div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
@@ -102,87 +106,22 @@
 						<div class="sb-nav-link-icon"><i class="fas fa-film"></i></div>
 						Movies
 					</a>
+				-->
 
 
 
-					<!-- ========================================================================================================== -->
 
-					<!-- 이전 코드 (3 dept, 2dept, 1dept) -->
-					<!-- ========================================================================================================== -->
-					<!--<div class="sb-sidenav-menu-heading">Profile</div>
-					<a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false"
-						aria-controls="collapseLayouts">
-						<div class="sb-nav-link-icon">
-							<i class="fas fa-columns"></i>
-						</div> Layouts
-						<div class="sb-sidenav-collapse-arrow">
-							<i class="fas fa-angle-down"></i>
-						</div>
-					</a>
-					<div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-						<nav class="sb-sidenav-menu-nested nav">
-							<a class="nav-link" href="layout-static.html">Static
-								Navigation</a> <a class="nav-link" href="layout-sidenav-light.html">Light
-								Sidenav</a>
-						</nav>
-					</div>
-					<a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false"
-						aria-controls="collapsePages">
-						<div class="sb-nav-link-icon">
-							<i class="fas fa-book-open"></i>
-						</div> Pages
-						<div class="sb-sidenav-collapse-arrow">
-							<i class="fas fa-angle-down"></i>
-						</div>
-					</a>
-					<div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
-						<nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-							<a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth"
-								aria-expanded="false" aria-controls="pagesCollapseAuth"> Authentication
-								<div class="sb-sidenav-collapse-arrow">
-									<i class="fas fa-angle-down"></i>
-								</div>
-							</a>
-							<div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne"
-								data-bs-parent="#sidenavAccordionPages">
-								<nav class="sb-sidenav-menu-nested nav">
-									<a class="nav-link" href="login.html">Login</a> <a class="nav-link" href="register.html">Register</a> <a
-										class="nav-link" href="password.html">Forgot Password</a>
-								</nav>
-							</div>
-							<a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError"
-								aria-expanded="false" aria-controls="pagesCollapseError"> Error
-								<div class="sb-sidenav-collapse-arrow">
-									<i class="fas fa-angle-down"></i>
-								</div>
-							</a>
-							<div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne"
-								data-bs-parent="#sidenavAccordionPages">
-								<nav class="sb-sidenav-menu-nested nav">
-									<a class="nav-link" href="401.html">401 Page</a> <a class="nav-link" href="404.html">404 Page</a> <a
-										class="nav-link" href="500.html">500 Page</a>
-								</nav>
-							</div>
-						</nav>
-					</div>
-
-					<div class="sb-sidenav-menu-heading">Storage</div>
-					<a class="nav-link" href="charts.html">
-						<div class="sb-nav-link-icon">
-							<i class="fas fa-chart-area"></i>
-						</div> Charts
-					</a> 
-					<a class="nav-link" href="tables.html">
-						<div class="sb-nav-link-icon">
-							<i class="fas fa-table"></i>
-						</div> Tables
-					</a>-->
-					<!-- ========================================================================================================== -->
+					
 				</div>
 			</div>
+			
 			<div class="sb-sidenav-footer">
 				<div class="small">Logged in as:</div>
 				Start Bootstrap
 			</div>
 		</nav>
 	</div>
+
+
+<script type="text/javascript" src="/jwtauth/js/utils.js"></script>
+<script type="text/javascript" src="/jwtauth/js/layout/left.js"></script>
