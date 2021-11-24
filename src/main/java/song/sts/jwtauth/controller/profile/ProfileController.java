bcefb839575983fbf2f31a44ca36597bf2bda419
songnumber1 <songnumber1@naver.com>
2021-11-24 17:59:27 +0900

    Date  - 2021-11-24 17:53
    Content
     - Menu Url 추가 및 변경
     - Controller, RestController에 RequestMapping 추가
     - SoftwareItem Entity 추가
      - Softwareitem에 dept와 menuid 컬럼 추가
    
    Todo
     - Software 데이터 추가하기
     - setting 메뉴 root, sub, last를 category와 동일하게 코드 구현
     - setting 사용자를 메뉴 category와 동일하게 코드 구현
     - setting 메뉴 role, active 저장 구현
     - setting 메뉴 account 구현
     - 좌측 메뉴 메뉴 role에 맞게끔 출력되도록 구현
     - footer 설정
     - storage 보기, 즐겨찾기 기능 구현

---
+++ src/main/java/song/sts/jwtauth/controller/profile/ProfileController.java
@@ -0,0 +1,15 @@
+package song.sts.jwtauth.controller.profile;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/profile")
+public class ProfileController {
+    @GetMapping("/")
+    public String profile(Model menuModel) {
+        return "profile/profile";
+    }
+}
